84a4b396995d5ff687ea16b4fc33580b05a4fc1c
Michael Mayer <michael@liquidbytes.net>
2017-10-20 08:34:52 +0200

    Set version to 0.4.3

--- tweethog.go
+++ tweethog.go
@@ -19,7 +19,7 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Usage = "Stream, filter and react to Twitter status updates"
-	app.Version = "0.4.2"
+	app.Version = "0.4.3"
 	app.Copyright = "Michael Mayer <michael@liquidbytes.net>"
 
 	app.Flags = cliFlags
